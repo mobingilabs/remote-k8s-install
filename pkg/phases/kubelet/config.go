b60e11b35778ae7ccbff668d0053f0b1eaad0420
n1ce37 <n1ce@n1cedeMac-mini.lan>
2019-05-24 17:49:02 +0800

    refactor certs pkg

--- pkg/phases/kubelet/config.go
+++ pkg/phases/kubelet/config.go
@@ -1,10 +1,12 @@
 package kubelet
 
 import (
-	kubeletconfig "k8s.io/kubernetes/pkg/kubelet/config"
+	kubeletconfigv1beta1 "k8s.io/kubelet/config/v1beta1"
 )
 
 // TODO
-func newKubeletConfig() *kubeletconfig.KubeletConfiguration {
-	return nil
+func newKubeletConfig() *kubeletconfigv1beta1.KubeletConfiguration {
+	return &kubeletconfigv1beta1.KubeletConfiguration{
+		Address: "0.0.0.0",
+	}
 }
