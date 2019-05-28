0919e5e79cd1229e91c55fc10b0aa84479822911
n1ce37 <n1ce@n1cedeMac-mini.lan>
2019-05-28 09:51:13 +0800

    static pod

--- pkg/phases/certs/certs_test.go
+++ pkg/phases/certs/certs_test.go
@@ -1,18 +1,1 @@
 package certs
-
-import (
-	"testing"
-
-	"github.com/stretchr/testify/assert"
-)
-
-func TestCreatePKIAssets(t *testing.T) {
-	o := Options{
-		InternalEndpoint: "192.168.1.1",
-		ExternalEndpoint: "47.40.40.1",
-		SANs:             []string{},
-	}
-	_, err := CreatePKIAssets(o)
-	assert.NoError(t, err, "create pki assests err:%v", err)
-
-}
